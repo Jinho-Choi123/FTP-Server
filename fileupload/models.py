d09a2a25302470c9021fa638e699df55bc6c5593
Jinho-Choi123 <choijinho817@gmail.com>
2021-09-04 13:52:06 +0000

    feature: Make Schema and Serializer for fileupload app

--- fileupload/models.py
+++ fileupload/models.py
@@ -1,12 +1,44 @@
 from django.db import models
 import datetime 
 import uuid
+
 # Create your models here.
-class File:
-    def __init(self, filename, ):
-        self.filename = filename 
-        self.uploadtime = datetime.datetime.now()
+class File(models.Model):
+    uid = models.UUIDField(
+        primary_key=True,
+        default = uuid.uuid4,
+        editable = False
+    )
+    gid = models.UUIDField(
+        primary_key = False,
+        editable = False,
+        null = True
+    )
+    content = models.FileField(max_length = 40, blank = False, null = True, upload_to = gid)
+
+    #initialize with filename, extension and groupid
+    def __init(self, gid):
+        self.gid = gid
+        #store path is <basepath>/gid/uid  저장 이름은 uid. 실제 이름은 filename.
+
+#File Group contains attr of list of File Obj
+class FileGroup(models.Model):
+    gid = models.UUIDField(
+        primary_key=True,
+        default = uuid.uuid4,
+        editable = False
+    )
+    uploadtime = models.DateTimeField(auto_now_add = True)
+    expiredtime = models.DateTimeField(null = True)
+    filenum = models.PositiveIntegerField(default = 0)
+    filesize = models.PositiveIntegerField(default = 0)
+    filelist = models.ManyToManyField(File)
+
+    def __init(self):
+        self.gid = uuid.uuid4()
+        #after expiredtime, the file will be nonactive.
         self.expiredtime = datetime.datetime.now() + datetime.timedelta(hours = 6)
-        self.uid = uuid.uuid4()
-        
+        #info of the numbers of files & total size of files. 
+        self.filenum = 0
+        self.filesize = 0
 
--- fileupload/serializers.py
+++ fileupload/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers 
+from fileupload.models import File, FileGroup
 
 # Serializer는 복잡한 데이터형식에서 데이터 추출을 도와준다. DB에 저장되어있는 데이터 <=> 필요한 데이터 추출!
 # 간단히 말하자면 파이썬 객체와 JSON/XML 변환기를 Serializer라고 생각하면 된다.
 
-class FileSerializer(serializers.ModelSerializer):
+class FileSerializer(serializers.Serializer):
+    uid = serializers.UUIDField(
+        format = 'hex'
+    )
+    gid = serializers.UUIDField(
+        format = 'hex'
+    )
+    content = serializers.FileField(
+        max_length = 40,
+        allow_empty_file = False,
+    )
+
+
+class FileGroupSerializer(serializers.ModelSerializer):
+    filelist = FileSerializer(read_only = True, many = True)
     class Meta:
-        model = 
+        model = FileGroup
+        fields = ['gid', 'expiredtime', 'filelist', 'filenum', 'filesize']
