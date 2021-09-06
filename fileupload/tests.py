ee5fc3378a09a8a426e19576ea7d62e06d5f48a1
Jinho-Choi123 <choijinho817@gmail.com>
2021-09-06 12:32:22 +0000

    test:make file upload testcase

--- fileupload/tests.py
+++ fileupload/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from ftp_server.settings import BASE_DIR
+import json
 
-# Create your tests here.
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from .models import File, FileGroup
+from .serializers import FileGroupSerializer, FileSerializer
+
+import logging
+logger = logging.getLogger(__name__)
+
+class FileUploadTestCase1(APITestCase):
+
+    def test_fileupload(self):
+        with open(BASE_DIR/'testfiles/video1.mp4', 'rb') as video1, open(BASE_DIR/'testfiles/video2.mp4', 'rb') as video2, open(BASE_DIR/'testfiles/image1.jpg', 'rb') as image1:
+            files = {
+                'uploadfile': video1,
+                'uploadfile': video2,
+                'uploadfile': image1,
+                }
+            response = self.client.post("/fileupload/", files = files, format='multipart')
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
