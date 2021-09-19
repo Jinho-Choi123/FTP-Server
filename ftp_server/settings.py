1a51b76829c5eadab1f3da300107a12335527de6
Jinho-Choi123 <choijinho817@gmail.com>
2021-09-19 05:43:02 +0000

    fix:allow frontend cors

--- ftp_server/settings.py
+++ ftp_server/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -139,3 +141,8 @@ TIME_ZONE = 'Asia/Seoul'
 USE_I18N = True 
 USE_L10N = True
 USE_TZ = False 
+
+#CORS SETTINGS
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
